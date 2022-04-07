e21ee9feb422869781cd0fbf9003b4d9197b519f
sediq <you@example.com>
2022-04-07 23:05:09 +0100

    Fixed connecting error bug and event memory leak

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const app = express();
 const server = http.createServer(app);
 const router = require("./modules/router");
 const mongoose = require("mongoose");
-const Controller = require("./modules/Controller");
 const URL = "mongodb://127.0.0.1/chatterbox";
-const Schema = require("./modules/Schema");
-const jwt = require("jsonwebtoken");
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
@@ -18,68 +15,34 @@ app.use(router);
 mongoose.connect(URL, (err) => {
   if (err) throw err;
   else {
-    server.listen(5000, console.log("Server on port 5000"));
+    server.listen(5000);
   }
 });
-// Handling socket jobs
-// io.use((socket, next) => {
-//   let $token = socket.handshake.auth;
-//   if ($token.senderToken) {
-//     jwt.verify($token.senderToken, "secret", (err, decoded) => {
-//       if (err) console.log(err);
-//       else {
-//         if ($token.accessToken) {
-//           // console.log("Logged in");
-//           next();
-//         } else {
-//           // Not logged in
-//           next();
-//         }
-//       }
-//     });
-//   } else {
-//     next(new Error("Thou shall not pass"));
-//   }
-// });
 
-// socket.on("join", async (link) => {
-//   // Fetching chats
-//   socket.emit("chats", []);
-//   socket.on("message", (msg) => {
-//     console.log(msg);
-//   });
-// await Schema.Link.findOne({ code: link }, (err, d) => {
-//   if (err || d == "" || d == null) {
-//     console.log("Nut found");
-//   } else {
-//     let { chats, creator } = d;
-//     socket.emit("chats", chats);
-//     socket.join(link);
-//     socket.on("message", (msg) => {
-//       console.log(msg);
-//       // console.log(cb);
-//     });
-//   }
-// });
-// });
-// });
 io.on("connection", (socket) => {
+  socket.emit("thru", true);
   socket.on("join", (link, cb) => {
     // Checking users
-    if (users.length > 2) cb(false);
-    else if (!users.includes(socket.handshake.auth.$token)) {
-      users.push(socket.handshake.auth.$token);
-      console.log(users);
-      cb(true);
-    } else {
-      cb(true);
-      console.log(users);
-    }
-    socket.join(link);
-    socket.on("message", (m, cb) => {
-      socket.broadcast.to(link).emit("message", m);
-      cb(m.message);
-    });
+    let payload = socket.handshake.auth.$token;
+    if (payload !== null) {
+      if (users.includes(payload)) cb(true);
+      else if (users.length > 2) cb(false);
+      else {
+        users.push(payload);
+        cb(true);
+      }
+      socket.join(link);
+      socket.on("message", (m, cb) => {
+        socket.broadcast.to(link).emit("broadcast", m);
+        cb(m);
+      });
+    } else cb(false);
   });
-  socket.on("disconnect", () => {});
+  socket.on("disconnect", (data) => {
+    let payload = socket.handshake.auth.$token;
+    if (users.includes(payload) === true) {
+      let r = users.filter((c) => c != payload);
+      users = r;
+    }
+  });
 });

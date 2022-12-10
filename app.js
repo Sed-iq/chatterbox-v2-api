a0de5d8cb378e29482d43fca242922de8ffa59b9
sediq <you@example.com>
2022-12-10 08:54:16 +0100

    Degrading to dummy to test

--- app.js
+++ app.js
@@ -3,76 +3,79 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 const dotenv = require('dotenv').config();
-const router = require("./modules/router");
+// const router = require("./modules/router");
 const mongoose = require("mongoose");
-const URL = "mongodb://127.0.0.1/chatterbox";
+const URL = process.env.DB_URL
 const Schema = require('./modules/Schema')
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
-});
-app.use(router);
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
+// app.use(router);
 mongoose.connect(URL, (err) => {
   if (err) throw err;
   else {
     server.listen(process.env.PORT, console.log("Server is running"));
   }
 });
-
-io.on("connection", (socket) => {
-  let payload = socket.handshake.auth.$token;
-  socket.emit("thru", true);
-  socket.on("join", async(link, cb) => {
-    // Checking users
-    Schema.Link.findOne({ code: link }, (err, d) =>{
-      if(err || d == " ") cb(false, false)
-      else {
-        if(d.onlineUsers.length < 2){
-          if(d.onlineUsers.includes(payload)){
-            cb(true, true)
-          }
-          else {
-            // Adding user 
-            d.onlineUsers.unshift(payload)
-            Schema.Link.findOneAndUpdate({ code:link }, { onlineUsers:d.onlineUsers } ,(err,x)=>{
-              if(err|| x =='') cb(false, false)
-                else {
-                  if(x.onlineUsers.length == 1) cb(true, true)
-                    else cb(true, false)   
-                }
-            })
-          }
-        }
-        else cb(false, false)
-      }
-    })
-       socket.join(link);
-  socket.broadcast.to(link).emit('online', true)
-       socket.on("message", (m, cb) => {
-       m.date = new Date()
-       socket.broadcast.to(link).emit("broadcast", m);
-       cb(m);
-      });  
-  });
-  socket.on("disconnect", (data) => {
- const $link = socket.handshake.auth.$link
-  Schema.Link.findOne({ code:$link })
-  .then(x => {
-      if(x == "") console.log('user not found')
-        else {
-          let n = x.onlineUsers.filter(c => c !== payload)
-          Schema.Link.findOneAndUpdate({ code : $link}, { onlineUsers: n }, (err) =>{
-            if(err) console.log(err)
-              else {
-              socket.broadcast.to($link).emit('online', false)
-          }
-          })
-        }
-  })  
-.catch(e =>{
-  console.log("Ending", e)
+app.get('/', ({}, res) =>{
+  res.send('Home page')
 })
-  });
-});
+
+// io.on("connection", (socket) => {
+//   let payload = socket.handshake.auth.$token;
+//   socket.emit("thru", true);
+//   socket.on("join", async(link, cb) => {
+//     // Checking users
+//     Schema.Link.findOne({ code: link }, (err, d) =>{
+//       if(err || d == " ") cb(false, false)
+//       else {
+//         if(d.onlineUsers.length < 2){
+//           if(d.onlineUsers.includes(payload)){
+//             cb(true, true)
+//           }
+//           else {
+//             // Adding user 
+//             d.onlineUsers.unshift(payload)
+//             Schema.Link.findOneAndUpdate({ code:link }, { onlineUsers:d.onlineUsers } ,(err,x)=>{
+//               if(err|| x =='') cb(false, false)
+//                 else {
+//                   if(x.onlineUsers.length == 1) cb(true, true)
+//                     else cb(true, false)   
+//                 }
+//             })
+//           }
+//         }
+//         else cb(false, false)
+//       }
+//     })
+//        socket.join(link);
+//   socket.broadcast.to(link).emit('online', true)
+//        socket.on("message", (m, cb) => {
+//        m.date = new Date()
+//        socket.broadcast.to(link).emit("broadcast", m);
+//        cb(m);
+//       });  
+//   });
+//   socket.on("disconnect", (data) => {
+//  const $link = socket.handshake.auth.$link
+//   Schema.Link.findOne({ code:$link })
+//   .then(x => {
+//       if(x == "") console.log('user not found')
+//         else {
+//           let n = x.onlineUsers.filter(c => c !== payload)
+//           Schema.Link.findOneAndUpdate({ code : $link}, { onlineUsers: n }, (err) =>{
+//             if(err) console.log(err)
+//               else {
+//               socket.broadcast.to($link).emit('online', false)
+//           }
+//           })
+//         }
+//   })  
+// .catch(e =>{
+//   console.log("Ending", e)
+// })
+//   });
+// });
 
